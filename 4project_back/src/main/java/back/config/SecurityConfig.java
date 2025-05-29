7a68bfc3e74fa61d9aba56e6ec84cfaf6d212c0a
Heachi-si <icingsugar001@naver.com>
2025-05-29 19:07:38 +0900

    156

--- 4project_back/src/main/java/back/config/SecurityConfig.java
+++ 4project_back/src/main/java/back/config/SecurityConfig.java
@@ -1,5 +1,5 @@
 package back.config;
-//afaf
+//156
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
