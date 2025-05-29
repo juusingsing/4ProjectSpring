c8be67e9cd74d02a9e2c7f5576b528c1edfe554e
Kim nayeon <kimnayeon4729@gmail.com>
2025-05-29 19:04:03 +0900

    165

--- 4project_back/src/main/java/back/config/SecurityConfig.java
+++ 4project_back/src/main/java/back/config/SecurityConfig.java
@@ -1,5 +1,5 @@
 package back.config;
-
+//afaf
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
