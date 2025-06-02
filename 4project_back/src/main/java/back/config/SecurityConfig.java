caa9383b99cc87ca57e4457923346b7d9721fb56
Hyunju <sc4880372@naver.com>
2025-06-02 12:34:24 +0900

    789

--- 4project_back/src/main/java/back/config/SecurityConfig.java
+++ 4project_back/src/main/java/back/config/SecurityConfig.java
@@ -1,5 +1,5 @@
 package back.config;
-//123456
+//123456789
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
