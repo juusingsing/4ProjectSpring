d8b865c5f4eeaa6952b48c97bd7c6d605aea0680
Kim nayeon <kimnayeon4729@gmail.com>
2025-06-13 17:42:35 +0900

    [김나연]캘린더 구현

--- 4project_back/src/main/java/back/controller/calendar/CalendarController.java
+++ 4project_back/src/main/java/back/controller/calendar/CalendarController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,5 +39,18 @@ public class CalendarController {
 		calendar.setUsersId(userDetails.getUser().getUsersId());
 		SecurityUtil.checkAuthorization(userDetails);
 		return ResponseEntity.ok(calendarService.selectLogsByDate(calendar));
+	}
+	@GetMapping("/animals")
+	public ResponseEntity<?> getUserAnimals(@AuthenticationPrincipal CustomUserDetails userDetails) {
+	    String usersId = userDetails.getUser().getUsersId();
+	    SecurityUtil.checkAuthorization(userDetails);
+	    return ResponseEntity.ok(calendarService.selectUserAnimals(usersId));
+	}
+
+	@GetMapping("/plants")
+	public ResponseEntity<?> getUserPlants(@AuthenticationPrincipal CustomUserDetails userDetails) {
+	    String usersId = userDetails.getUser().getUsersId();
+	    SecurityUtil.checkAuthorization(userDetails);
+	    return ResponseEntity.ok(calendarService.selectUserPlants(usersId));
 	}
 }
